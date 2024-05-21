093220ba9e57d7e2e16163f055593407d9a0c67b
Youngjoo <my.cat.liang@gmail.com>
2024-05-21 11:01:44 +0900

    initial commit

---
+++ DeviceDriver/test.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+#include "DeviceDriver.h"
+
+class DeviceDriverTest : public testing::Test
+{
+protected:
+
+};
+TEST_F(DeviceDriverTest, Read5TimesAndVerifyData) {
+
+}
+
+TEST_F(DeviceDriverTest, VerifyEmptyBeforeWirte) {
+
+}
