79b4a045278cdcd5f75cc3e8d9daf1bef5117b9c
Youngjoo <my.cat.liang@gmail.com>
2024-05-21 11:04:06 +0900

    [feature] - add read test

--- DeviceDriver/DeviceDriver.cpp
+++ DeviceDriver/DeviceDriver.cpp
@@ -1,12 +1,22 @@
 #include "DeviceDriver.h"
+#include <stdexcept>
+
+class ReadFailException : public std::exception {
+
+};
 
 DeviceDriver::DeviceDriver(FlashMemoryDevice* hardware) : m_hardware(hardware)
 {}
 
 int DeviceDriver::read(long address)
 {
-    // TODO: implement this method properly
-    return (int)(m_hardware->read(address));
+    auto firstResult = m_hardware->read(address);
+    for (int i = 0; i < 4; i++)
+    {
+        if (firstResult != m_hardware->read(address))
+            throw ReadFailException();
+    }
+    return firstResult;
 }
 
 void DeviceDriver::write(long address, int data)
--- DeviceDriver/test.cpp
+++ DeviceDriver/test.cpp
@@ -2,13 +2,23 @@
 #include "gmock/gmock.h"
 #include "DeviceDriver.h"
 
+class FlashMemoryDeviceMock : public FlashMemoryDevice
+{
+public:
+	MOCK_METHOD(unsigned char, read, (long), (override));
+	MOCK_METHOD(void, write, (long, unsigned char), (override));
+};
+
 class DeviceDriverTest : public testing::Test
 {
 protected:
-
+	FlashMemoryDeviceMock deviceMock{};
+	DeviceDriver driver{ &deviceMock };
 };
 TEST_F(DeviceDriverTest, Read5TimesAndVerifyData) {
-
+	EXPECT_CALL(deviceMock, read(testing::_)).Times(5);
+	constexpr long TEST_ADDRESS = 0x1234;
+	driver.read(TEST_ADDRESS);
 }
 
 TEST_F(DeviceDriverTest, VerifyEmptyBeforeWirte) {
