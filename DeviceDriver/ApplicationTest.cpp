d62f42ab7582911d0273c3225b0bf1a690e6baae
Youngjoo <my.cat.liang@gmail.com>
2024-05-21 12:04:10 +0900

    add skeleton for Application

---
+++ DeviceDriver/ApplicationTest.cpp
@@ -0,0 +1,16 @@
+#include "gtest/gtest.h"
+#include "gmock/gmock.h"
+
+class ApplicationTest : public testing::Test
+{
+protected:
+
+};
+
+TEST_F(ApplicationTest, ReadAndPrint) {
+
+}
+
+TEST_F(ApplicationTest, WriteAll) {
+
+}
